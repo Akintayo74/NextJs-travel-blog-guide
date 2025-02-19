2770f8d6b649b21c959add8c7b32c2daf1361835
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-02-19 15:44:07 +0000

    Made progress with footer. Restructuring container

--- components/Footer.js
+++ components/Footer.js
@@ -5,43 +5,48 @@ const Footer = () => {
         <div className={styles.footer}>
 
             <div className={styles.footer__info}>
-                <div className={styles.footer__aboutSection}>
-                    <h3>About</h3>
-                    <div className={styles.footer__about}>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut rutrum purus. Etiam lacinia quam at feugiat semper. Mauris imperdiet eros pulvinar massa convallis, vel lacinia nibh pharetra. Vestibulum gravida.</p>
+                <div className={styles.footer__infoSection}>
+                    <div className={styles.footer__aboutSection}>
+                        <div className={styles.footer__about}>
+                            <h3>About</h3>
+                            <span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut rutrum purus. Etiam lacinia quam at feugiat semper. Mauris imperdiet eros pulvinar massa convallis, vel lacinia nibh pharetra. Vestibulum gravida.</p>
+                            </span>
+                        </div>
+                        <div className={styles.footer__contact}>
+                            <p><span>Email: </span>info@jstemplate.net</p>
+                            <p><span>Phone: </span>880 123 456 789</p>
+                        </div>
                     </div>
-                    <div className={styles.footer__contact}>
 
-                    </div>
-                </div>
-
-                <div className={styles.footer__links}>
-                    <div className={styles.footer__quickLinks}>
-                        <h3>Quick link</h3>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Blog</li>
-                            <li>Archived</li>
-                            <li>Author</li>
-                            <li>Contact</li>
-                        </ul>
-                    </div>
-                    <div className={styles.footer__category}>
-                        <h3>Category</h3>
-                        <ul>
-                            <li>Lifestyle</li>
-                            <li>Technology</li>
-                            <li>Travel</li>
-                            <li>Business</li>
-                            <li>Economy</li>
-                            <li>Sports</li>
-                        </ul>
+                    <div className={styles.footer__links}>
+                        <div className={styles.footer__quickLinks}>
+                            <h3>Quick link</h3>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Blog</li>
+                                <li>Archived</li>
+                                <li>Author</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                        <div className={styles.footer__category}>
+                            <h3>Category</h3>
+                            <ul>
+                                <li>Lifestyle</li>
+                                <li>Technology</li>
+                                <li>Travel</li>
+                                <li>Business</li>
+                                <li>Economy</li>
+                                <li>Sports</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
                 <div className={styles.footer__newsletter}>
-                    <div>
+                    <div className={styles.footer__newsletterHeading}>
                         <h2>Weekly Newsletter</h2>
                         <span>Get blog articles and offers via email</span>
                     </div>
