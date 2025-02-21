8766aa975d17ca9d84ad755edfb2ac2c9bd321d1
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-02-21 23:46:16 +0000

    Rounding up MainImagae component

--- components/MainImage.js
+++ components/MainImage.js
@@ -12,6 +12,25 @@ export default function MainImage(){
                 alt='woman with laptop'
                 priority
             />
+            <div className={styles.MainImage__text}>
+                <div className={styles.MainImage__header}>
+                    <span className={styles.main__stacked_header}>Technology</span>
+                    <h2>The Impact of Technology on the Workplace: How Technology Is Changing</h2>
+                </div>
+
+                <div className={styles.main__author}>
+                    <div className={styles.main__author_avatar}>
+                        <Image
+                            src='/images/avatarBlank.svg'
+                            width={60}
+                            height={60}
+                            alt='Blank avatar'
+                        />
+                        <span>Jason Francisco</span>
+                    </div>
+                    <span>August 20, 2022</span>
+                </div>
+            </div>
         </div>
     )
 }
