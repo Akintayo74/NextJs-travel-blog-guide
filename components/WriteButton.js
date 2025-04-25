1bd9c44e628bac736d2661acc5027ea337f3bf3b
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-04-25 15:52:38 +0000

    Modified WriteButton.js to link to /blogs/create

--- components/WriteButton.js
+++ components/WriteButton.js
@@ -5,8 +5,12 @@ import styles from '@/styles/WriteButton.module.css'
 export default function WriteButton(){
     const router = useRouter();
 
+    const handleClick = () => {
+        router.push('/blog/create')
+    }
+
     return (
-        <div className={styles.write__container}>
+        <div className={styles.write__container} onClick={handleClick}>
             <NotebookPen size={24}/>
         </div>
     )
