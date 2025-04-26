77b39811ff2cc731535da9a83f2978f6ea31c7db
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-04-26 06:50:45 +0000

    Fixed redirection problem with WriteButton.js
    The problem stemmed from WithAuth.js file and how it handled redirection, checking for localStorage token before it has loaded. Fixed that, now it waits until localStorage check is done before the page is rendered

--- components/withAuth.js
+++ components/withAuth.js
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function withAuth(Component) {
     return function AuthenticatedComponent(props) {
         const router = useRouter();
+        const [isAuthenticated, setIsAuthenticated] = useState(null);
 
         useEffect(() => {
             const user = JSON.parse(localStorage.getItem('user'));
-            if (!user){
-                router.push('/auth/login');
+            if (user){
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+                router.push('/auth/login')
             }
-        }, [])
+        }, [router]);
+
+        if (isAuthenticated === null) {
+            return <div>Loading...</div>;
+        }
+
+        if (isAuthenticated === false) {
+            return null;
+        }
 
         return <Component {...props} />
     }
--- src/pages/blog/create.js
+++ src/pages/blog/create.js
@@ -1,13 +1,10 @@
 // /pages/blog/create.js
 import CreateBlog from 'components/CreateBlog';
 import withAuth from 'components/withAuth';
-import Layout from 'components/Layout'; // Assuming you have a layout component
 
 const CreateBlogPage = () => {
   return (
-    // <Layout>
     <CreateBlog />
-    // </Layout>
   );
 };
 
