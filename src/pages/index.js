d1a7b7c4f0496350b8c892149867c5d3774ef888
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-05-03 16:23:02 +0000

    Changed input background color
    For Login and SignUp pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import Main from '../../components/Main';
 import Advert from '../../components/Advert';
 import LatestPosts from '../../components/LatestPosts';
-// import FloatingWriteButton from '../../components/WriteButton';
 
 
 export default function Home(){
   return(
     <div>
-      {/* <FloatingWriteButton /> */}
       <Main />
       <Advert />
       <LatestPosts />
