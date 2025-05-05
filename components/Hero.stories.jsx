3f3374eadc5240add68a5705afca77ebc7b2ae4a
akinnibosunakintayo74 <akinnibosunakintayo74@gmail.com>
2025-05-05 15:13:42 +0000

    Storybook is working!
    Want to edit the /storybook/preview.js file next to load fonts in stories as well.

--- components/Hero.stories.jsx
+++ components/Hero.stories.jsx
@@ -1,8 +1,26 @@
 import Hero from './Hero';
+import { ThemeProvider } from 'components/ThemeContext'; // Make sure the path is correct
+import { AuthProvider } from 'context/AuthContext'; // Make sure the path is correct
 
 export default {
-    title: 'components/Hero',
-    component: Hero,
+  title: 'components/Hero',
+  component: Hero,
 };
 
-export const Default = () => <Hero />
+const mockTheme = {
+  theme: 'light', // or 'dark' depending on the theme you want to mock
+  toggleTheme: () => {}, // Mocked toggle function
+};
+
+const mockAuth = {
+  user: null, // or mock a user object, e.g., { name: 'John Doe' }
+  logout: () => {}, // Mock logout function
+};
+
+export const Default = () => (
+  <ThemeProvider value={mockTheme}>
+    <AuthProvider value={mockAuth}>
+      <Hero />
+    </AuthProvider>
+  </ThemeProvider>
+);
